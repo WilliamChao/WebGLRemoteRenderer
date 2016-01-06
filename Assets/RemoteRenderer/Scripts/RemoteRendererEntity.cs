5606ce8f57c30ae866cfea7e846c0da7f8fea64a
i-saint <saint.skr@gmail.com>
2016-01-06 22:48:48 +0900

    wip

--- Assets/RemoteRenderer/Scripts/RemoteRendererEntity.cs
+++ Assets/RemoteRenderer/Scripts/RemoteRendererEntity.cs
@@ -1,20 +1,51 @@
-﻿using System.Collections;
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 
 [AddComponentMenu("RemoteRenderer/RemoteRendererEntity")]
 public class RemoteRendererEntity : MonoBehaviour
 {
+    static HashSet<RemoteRendererEntity> s_instances;
+    public static HashSet<RemoteRendererEntity> GetInstances()
+    {
+        if(s_instances == null)
+        {
+            s_instances = new HashSet<RemoteRendererEntity>();
+        }
+        return s_instances;
+    }
+
     Transform m_trans;
     Camera m_camera;
     MeshRenderer m_mesh_renderer;
     SkinnedMeshRenderer m_skmesh_renderer;
+    RemoteRenderer.EntityData m_data;
 
-    void Start()
+
+    public void RRSerialize(BinaryWriter io)
     {
+        m_data.position = m_trans.position;
+        m_data.rotation = m_trans.rotation;
+        m_data.scale = m_trans.lossyScale;
+
+        // todo write
+    }
+
+
+    void OnEnable()
+    {
+        GetInstances().Add(this);
+
         m_trans = GetComponent<Transform>();
         m_camera = GetComponent<Camera>();
         m_mesh_renderer = GetComponent<MeshRenderer>();
         m_skmesh_renderer = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    void OnDisable()
+    {
+        GetInstances().Remove(this);
     }
 }
--- Assets/RemoteRenderer/Scripts/RemoteRendererServer.cs
+++ Assets/RemoteRenderer/Scripts/RemoteRendererServer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System.IO;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -6,4 +7,79 @@ using UnityEngine.Networking;
 [AddComponentMenu("RemoteRenderer/RemoteRendererServer")]
 public class RemoteRendererServer : MonoBehaviour
 {
+    public enum Type
+    {
+        WebSocket,
+        Normal,
+    }
+    Type m_type = Type.WebSocket;
+    public int m_port = 14841;
+    public int m_maxConnections = 20;
+
+    int m_chID;
+    int m_sockID;
+    int m_connectionID;
+    bool m_started = false;
+
+    MemoryStream m_stream;
+    BinaryWriter m_serializer;
+
+
+    public bool RRStartServer()
+    {
+        if(m_started)
+        {
+            Debug.Log("RemoteRendererServer: already started.");
+            return false;
+        }
+
+        NetworkTransport.Init();
+        ConnectionConfig config = new ConnectionConfig();
+        m_chID = config.AddChannel(QosType.Reliable);
+        HostTopology topology = new HostTopology(config, m_maxConnections);
+        if(m_type == Type.Normal)
+        {
+            m_sockID = NetworkTransport.AddHost(topology, m_port);
+        }
+        else if(m_type == Type.WebSocket)
+        {
+            m_sockID = NetworkTransport.AddWebsocketHost(topology, m_port);
+        }
+
+        byte error;
+        m_connectionID = NetworkTransport.Connect(m_sockID, "localhost", m_port, 0, out error);
+        Debug.Log("Connected to server. ConnectionId: " + m_connectionID);
+
+        m_stream = new MemoryStream(1024*64);
+        m_serializer = new BinaryWriter(m_stream);
+        m_started = true;
+        return true;
+    }
+
+    public void RRStopServer()
+    {
+        // todo
+        //NetworkTransport.DisconnectNetworkHost();
+    }
+
+    public void RRUpdate()
+    {
+        if(!m_started) { return; }
+
+        foreach(var e in RemoteRendererEntity.GetInstances())
+        {
+            e.RRSerialize(m_serializer);
+        }
+        m_serializer.Flush();
+
+        var buf = m_stream.ToArray();
+        byte error;
+        NetworkTransport.Send(m_sockID, m_connectionID, m_chID, buf, buf.Length, out error);
+    }
+
+
+    void Start()
+    {
+        RRStartServer();
+    }
 }
